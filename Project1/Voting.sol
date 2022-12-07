3e0c7efe4432fc6c0c7a9ce2dedb445785fb42df
y-chi <yannick.chi@gmail.com>
2022-12-07 21:09:10 +0100

    Add Project1

---
+++ Project1/Voting.sol
@@ -0,0 +1,140 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+error Voting__WrongStatus();
+error Voting__VoterNotRegistered();
+error Voting__AlreadyVoted();
+error Voting__ProposalDoesNotExist();
+error Voting__AlreadyAtLastStep();
+
+/// @author Yannick C.
+/// @title A Voting system smart contract
+contract Voting is Ownable {
+    struct Voter {
+        bool isRegistered;
+        bool hasVoted;
+        uint256 votedProposalId;
+    }
+    struct Proposal {
+        string description;
+        uint256 voteCount;
+    }
+    enum WorkflowStatus {
+        RegisteringVoters,
+        ProposalsRegistrationStarted,
+        ProposalsRegistrationEnded,
+        VotingSessionStarted,
+        VotingSessionEnded,
+        VotesTallied
+    }
+
+    mapping(address => Voter) public whitelistedVoters;
+    Proposal[] public proposals;
+    uint256 private winningProposalId;
+    WorkflowStatus public status;
+
+    event VoterRegistered(address voterAddress);
+    event WorkflowStatusChange(
+        WorkflowStatus previousStatus,
+        WorkflowStatus newStatus
+    );
+    event ProposalRegistered(uint256 proposalId);
+    event Voted(address voter, uint256 proposalId);
+
+    modifier onlyVotingStatus(WorkflowStatus _status) {
+        if (status != _status) {
+            revert Voting__WrongStatus();
+        }
+        _;
+    }
+    modifier onlyWhitelisted() {
+        if (!whitelistedVoters[msg.sender].isRegistered) {
+            revert Voting__VoterNotRegistered();
+        }
+        _;
+    }
+
+    /// Go to next WorkflowStatusChange status
+    function goToNextStep() external onlyOwner {
+        if (status == type(WorkflowStatus).max) {
+            revert Voting__AlreadyAtLastStep();
+        }
+        WorkflowStatus previousStatus = status;
+        status = WorkflowStatus(uint256(status) + 1);
+        emit WorkflowStatusChange(previousStatus, status);
+    }
+
+    /// Whitelist a Voter
+    /// @param _voter Voter to add to the whitelist
+    function registerVoter(address _voter)
+        external
+        onlyOwner
+        onlyVotingStatus(WorkflowStatus.RegisteringVoters)
+    {
+        whitelistedVoters[_voter].isRegistered = true;
+        emit VoterRegistered(_voter);
+    }
+
+    /// Voter can register their proposals
+    /// @param _description description of the proposal
+    function registerProposal(string memory _description)
+        external
+        onlyWhitelisted
+        onlyVotingStatus(WorkflowStatus.ProposalsRegistrationStarted)
+    {
+        Proposal memory proposal = Proposal(_description, 0);
+        uint256 proposalId = proposals.length;
+        proposals.push(proposal);
+        emit ProposalRegistered(proposalId);
+    }
+
+    /// Voters vote for their preferred proposal
+    /// @param _proposalId id of the proposal to vote for
+    function voteForProposal(uint256 _proposalId)
+        external
+        onlyWhitelisted
+        onlyVotingStatus(WorkflowStatus.VotingSessionStarted)
+    {
+        if (_proposalId >= proposals.length) {
+            revert Voting__ProposalDoesNotExist();
+        }
+        if (whitelistedVoters[msg.sender].hasVoted) {
+            revert Voting__AlreadyVoted();
+        }
+        proposals[_proposalId].voteCount++;
+        whitelistedVoters[msg.sender].hasVoted = true;
+        whitelistedVoters[msg.sender].votedProposalId = _proposalId;
+        emit Voted(msg.sender, _proposalId);
+    }
+
+    /// Admin counts the votes
+    function countVotes()
+        external
+        onlyOwner
+        onlyVotingStatus(WorkflowStatus.VotingSessionEnded)
+    {
+        uint256 tempWinningId;
+        for (uint256 currentId = 1; currentId < proposals.length; currentId++) {
+            if (
+                proposals[currentId].voteCount >
+                proposals[currentId - 1].voteCount
+            ) {
+                tempWinningId = currentId;
+            }
+        }
+        winningProposalId = tempWinningId;
+        this.goToNextStep();
+    }
+
+    /// Get the winning proposal
+    function getWinner()
+        external
+        view
+        onlyVotingStatus(WorkflowStatus.VotesTallied)
+        returns (Proposal memory)
+    {
+        return proposals[winningProposalId];
+    }
+}
